87df7a7731d5247772e3e08c289cda50979f9e24
gouthamm91 <101041477+gouthamm91@users.noreply.github.com>
2024-05-12 15:12:58 +0530

    changed the delim to + from space

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ def homePage():
 	<a style="font-size: 200%; font-weight: bold; color: red;">VPN</a> <a style="font-size: 200%; font-weight: bold;">KILLER</a><br>
 	<input id="inp" style="border-radius: 5px; border: 1px solid grey; font-size: 105%; padding: 3px; margin: 5px; width: 99%;">
 
-	<input type="button" style="border-radius: 5px; border: 1px solid grey; font-size: 200%;; padding: 3px; margin: 5px; width: 99.5%; height: 100px;" value="Search Now..." onclick="window.open('/search/'+inp.value, target='_blank');">
+	<input type="button" style="border-radius: 5px; border: 1px solid grey; font-size: 200%;; padding: 3px; margin: 5px; width: 99.5%; height: 100px;" value="Search Now..." onclick="window.open('/search/'+inp.value.replace(/ /g, '+'), target='_blank');">
 
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="PornHub"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="XVideos"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="XHamster"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="BeeG"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="You-Porn"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="MilfFox"/>
-	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value+' '+this.value.toLowerCase(), target='_blank');" value="Spankbang"/>
-	<!-- <input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.location.href='/search/'+inp.value;" value=""/>
+	<!-- <input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="PornHub"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="XVideos"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="XHamster"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="BeeG"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="You-Porn"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="MilfFox"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.open('/search/'+inp.value.replace(/ /g, '+')+ '+' +this.value.toLowerCase().replace(/ /g, '+'), target='_blank');" value="Spankbang"/>
+	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.location.href='/search/'+inp.value;" value=""/>
 	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.location.href='/search/'+inp.value;" value=""/>
 	<input type="button" style="font-size: 200%; width: 175px; height: 100px; border-radius: 10px; border: 1px solid grey;" onclick="window.location.href='/search/'+inp.value;" value=""/> -->
 </body>
