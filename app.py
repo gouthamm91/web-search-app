0933257514a847951bf4ec8ae81b6e1de9f506d4
gouthamm91 <101041477+gouthamm91@users.noreply.github.com>
2024-05-12 15:27:18 +0530

    onchange added

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def homePage():
 <body style="font-family: sans-serif; background-color: rgba(200, 200, 200, 1)">
 	<center>
 	<a style="font-size: 200%; font-weight: bold; color: red;">VPN</a> <a style="font-size: 200%; font-weight: bold;">KILLER</a><br>
-	<input id="inp" style="border-radius: 5px; border: 1px solid grey; font-size: 105%; padding: 3px; margin: 5px; width: 99%;">
+	<input id="inp" style="border-radius: 5px; border: 1px solid grey; font-size: 105%; padding: 3px; margin: 5px; width: 99%;" onchange="window.open('/search/'+inp.value.replace(/ /g, '+'), target='_blank');"/>
 
 	<input type="button" style="border-radius: 5px; border: 1px solid grey; font-size: 200%;; padding: 3px; margin: 5px; width: 99.5%; height: 100px;" value="Search Now..." onclick="window.open('/search/'+inp.value.replace(/ /g, '+'), target='_blank');">
 
